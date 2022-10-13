e316a76e2504d4d9fc7016897f30f4f4a2457e12
Stone Preston <stone.preston@canonical.com>
2022-10-13 14:29:21 -0500

    remove wait

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -258,8 +258,6 @@ def kubectl_exec(kubectl):
 @pytest.fixture(scope="module")
 async def k8s_storage(kubectl):
     await kubectl("apply", "-f", "tests/data/vsphere-storageclass.yaml")
-    log.info("waiting 2 minutes for storage ...")
-    await asyncio.sleep(120)
 
 
 @pytest.fixture(scope="module")
