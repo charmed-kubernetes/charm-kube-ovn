be4092c4d93d09ae911841becf64c1df8224f98b
George Kraft <george.kraft@canonical.com>
2023-05-16 16:48:02 -0500

    Update to kube-ovn v1.11.5 (#44)
    * Update to kube-ovn v1.11.3
    
    * Fix unit test failures and related bugs
    
    * Remove python 3.7 from test matrix
    
    * Update integration workflow to juju 3.1 and python 3.10
    
    * Update juju scp call for juju 3.x
    
    * Actually use python 3.10
    
    * Use non-hidden basetemp directory for juju 3.x support
    
    * Use jammy controller
    
    * Use basetemp path that works in github runners
    
    * Fix kubeconfig filename
    
    * Update kubernetes-core channel
    
    * Check results from juju deploy call
    
    * Replace `juju run` with `juju exec`
    
    * Copy kubectl-ko plugin to temp path for juju 3.x support
    
    * Drop JAMMY_BOOTSTRAP_OPTIONS
    
    * Clean up logging-config arg
    
    * Rewrite test_bgp to use natless subnet
    
    * Revert "Clean up logging-config arg"
    
    This reverts commit 86d6ed18e4dd496a4af3fec2b44063869d1de7bf.
    
    * Revert "Drop JAMMY_BOOTSTRAP_OPTIONS"
    
    This reverts commit e42800dd76fe211370a4e92c32fef2ad76fe8c37.
    
    * test_multi_nic_ipam: delete pod and subnet before net-attach-def
    
    * Mark test_bgp and test_external_gateway as xfail
    
    * Add vsphere-cloud-provider to fix test_prometheus
    
    * Bump prometheus wait timeout
    
    * Fix prometheus.is_ready failure due to string change
    
    * Update expected prometheus metrics
    
    * Fix PV stuck due to VM hardware revision
    
    * Fix Prometheus.is_ready exception when connection refused
    
    * Update to kube-ovn 1.11.5
    
    * Revert "Mark test_bgp and test_external_gateway as xfail"
    
    This reverts commit 997e4af8956d61d32e1a7a5d8e3f8f6aabcdd9d3.
    
    * Add test_pod_mirror workaround, sleep 20 seconds
    
    * Use json.dumps to render enable-mirror arg
    
    Co-authored-by: Adam Dyess <adam.dyess@canonical.com>
    
    ---------
    
    Co-authored-by: Adam Dyess <adam.dyess@canonical.com>

--- src/charm.py
+++ src/charm.py
@@ -104,7 +104,7 @@ class KubeOvnCharm(CharmBase):
 
     def apply_crds(self):
         self.unit.status = MaintenanceStatus("Applying CRDs")
-        self.kubectl("apply", "-f", "templates/crd.yaml")
+        self.kubectl("apply", "-f", "templates/kube-ovn/kube-ovn-crd.yaml")
 
     def apply_grafana_agent(self, remote_endpoints):
         namespace = self.stored.grafana_namespace
@@ -135,7 +135,8 @@ class KubeOvnCharm(CharmBase):
 
     def apply_kube_ovn(self, service_cidr, registry):
         self.unit.status = MaintenanceStatus("Applying Kube-OVN resources")
-        resources = self.load_manifest("kube-ovn.yaml")
+        resources = self.load_manifest("kube-ovn/kube-ovn.yaml")
+        control_plane_node_label = self.model.config["control-plane-node-label"]
         cidr = self.model.config["default-cidr"]
         gateway = self.model.config["default-gateway"]
         pinger_address = self.model.config["pinger-external-address"]
@@ -151,6 +152,11 @@ class KubeOvnCharm(CharmBase):
         kube_ovn_controller = self.get_resource(
             resources, kind="Deployment", name="kube-ovn-controller"
         )
+        self.set_replicas(kube_ovn_controller, len(node_ips))
+        self.set_node_selector(
+            kube_ovn_controller, control_plane_node_label, replace="kube-ovn/role"
+        )
+
         kube_ovn_controller_container = self.get_container_resource(
             kube_ovn_controller, container_name="kube-ovn-controller"
         )
@@ -162,6 +168,9 @@ class KubeOvnCharm(CharmBase):
                 "--service-cluster-ip-range": service_cidr,
                 "--node-switch-cidr": node_switch_cidr,
             },
+        )
+        self.replace_container_env_vars(
+            kube_ovn_controller_container, env_vars={"OVN_DB_IPS": ",".join(node_ips)}
         )
 
         self.add_container_args(
@@ -184,7 +193,7 @@ class KubeOvnCharm(CharmBase):
             )
         else:
             # Only enable the mirror if a mirror interface is also provided
-            cni_args_to_replace["--enable-mirror"] = enable_global_mirror
+            cni_args_to_replace["--enable-mirror"] = json.dumps(enable_global_mirror)
         self.replace_container_args(cni_server_container, args=cni_args_to_replace)
 
         cni_args_to_add = {}
@@ -210,10 +219,10 @@ class KubeOvnCharm(CharmBase):
         kube_ovn_monitor = self.get_resource(
             resources, kind="Deployment", name="kube-ovn-monitor"
         )
-        self.replace_node_selector(
+        self.set_node_selector(
             kube_ovn_monitor,
-            self.model.config["control-plane-node-label"],
-            "kube-ovn/role",
+            control_plane_node_label,
+            replace="kube-ovn/role",
         )
         self.set_replicas(kube_ovn_monitor, len(node_ips))
 
@@ -225,15 +234,17 @@ class KubeOvnCharm(CharmBase):
 
     def apply_ovn(self, registry):
         self.unit.status = MaintenanceStatus("Applying OVN resources")
-        resources = self.load_manifest("ovn.yaml")
+        resources = self.load_manifest("kube-ovn/ovn.yaml")
         node_ips = self.get_ovn_node_ips()
         self.replace_images(resources, registry)
 
         ovn_central = self.get_resource(
             resources, kind="Deployment", name="ovn-central"
         )
-        self.replace_node_selector(
-            ovn_central, self.model.config["control-plane-node-label"], "kube-ovn/role"
+        self.set_node_selector(
+            ovn_central,
+            self.model.config["control-plane-node-label"],
+            replace="kube-ovn/role",
         )
         self.set_replicas(ovn_central, len(node_ips))
 
@@ -244,11 +255,19 @@ class KubeOvnCharm(CharmBase):
             ovn_central_container, env_vars={"NODE_IPS": ",".join(node_ips)}
         )
 
+        ovs_ovn = self.get_resource(resources, kind="DaemonSet", name="ovs-ovn")
+        openvswitch_container = self.get_container_resource(
+            ovs_ovn, container_name="openvswitch"
+        )
+        self.replace_container_env_vars(
+            openvswitch_container, env_vars={"OVN_DB_IPS": ",".join(node_ips)}
+        )
+
         self.apply_manifest(resources, "ovn.yaml")
 
     def apply_speaker(self, registry, speaker_config: SpeakerConfig):
         self.unit.status = MaintenanceStatus("Applying Speaker resource")
-        resources = self.load_manifest("speaker.yaml")
+        resources = self.load_manifest("kube-ovn/speaker.yaml")
         speaker = self.get_resource(
             resources, kind="DaemonSet", name="kube-ovn-speaker"
         )
@@ -271,8 +290,8 @@ class KubeOvnCharm(CharmBase):
             },
         )
         self.replace_images(resources, registry)
-        self.replace_node_selector(
-            speaker, speaker_config.node_selector, "ovn.kubernetes.io/bgp"
+        self.set_node_selector(
+            speaker, speaker_config.node_selector, replace="ovn.kubernetes.io/bgp"
         )
         self.replace_name(speaker, speaker_config.name)
         self.apply_manifest(resources, f"{speaker_config.name}.speaker.yaml")
@@ -605,13 +624,6 @@ class KubeOvnCharm(CharmBase):
                             [registry] + container["image"].split("/")[-2:]
                         )
 
-    def replace_node_selector(self, resource, new_label, key_to_replace):
-        label_key, label_value = new_label.split("=")
-
-        node_selector = resource["spec"]["template"]["spec"]["nodeSelector"]
-        del node_selector[key_to_replace]
-        node_selector[label_key] = label_value
-
     def replace_name(self, resource, new_name):
         resource["metadata"]["name"] = new_name
 
@@ -646,6 +658,14 @@ class KubeOvnCharm(CharmBase):
     def set_active_status(self):
         if self.stored.kube_ovn_configured:
             self.unit.status = ActiveStatus()
+
+    def set_node_selector(self, resource, new_label, replace=None):
+        label_key, label_value = new_label.split("=")
+
+        node_selector = resource["spec"]["template"]["spec"]["nodeSelector"]
+        if replace and replace in node_selector:
+            del node_selector[replace]
+        node_selector[label_key] = label_value
 
     def set_replicas(self, resource, replicas):
         resource["spec"]["replicas"] = replicas
